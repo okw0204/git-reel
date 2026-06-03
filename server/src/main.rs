4b1ed7fef4f505555e1f88570d0ea7a3253a8cba
okw <okw0204@gmail.com>
2026-06-03 09:38:50 +0900

    feat: load env file for server

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,6 +3,8 @@ use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let _ = dotenvy::dotenv();
+
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .init();
